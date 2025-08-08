8dbb44f417ad6783cb0c9924daa928ab5b1962e5
AltinGashi1882 <altingashi1882@gmail.com>
2025-08-09 00:30:20 +0200

    New Update
    Weather Changes

--- js_2/weather.js
+++ js_2/weather.js
@@ -1,39 +1,23 @@
 const apiKey = "03f7860b678c3b369067db09e27c7363";
 const weatherFooter = document.getElementById("weather-footer");
 
-function getWeather(lat, lon) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
     fetch(url)
-    .then(respone => respone.json())
-    .then(data => {
-        const location = data.name;
-        const temp = data.main.temp.toFixed(1);
-        const description = data.weather[0].description;
-        const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        .then(response => response.json())
+        .then(data => {
+            const location = data.name;
+            const temp = data.main.temp.toFixed(1);
+            const description = data.weather[0].description;
+            const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
-        weatherFooter.innerHTML = `<img src="${icon}" alt="${description}"> ${location}: ${temp}°C`;
-    })
-    .catch(() => {
-        weatherFooter.textContent = "Unable to load weather";
-    });
+            weatherFooter.innerHTML = `<img src="${icon}" alt="${description}" style="width:40px; vertical-align:middle;"> ${location}: ${temp}°C`;
+        })
+        .catch(() => {
+            weatherFooter.textContent = "Unable to load weather";
+        });
 }
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => 
-        {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            getWeather(lat, lon);
-        },
-         () => {
-            weatherFooter.textContent = "Location access denied";
-         }
-    );
-    } else {
-        weatherFooter.textContent = "Geolocatio not supported";
-    }
-}
-
-getLocation();
+// Call with a fixed city
+getWeather("Prizren");
